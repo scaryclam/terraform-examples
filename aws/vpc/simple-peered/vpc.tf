7af72ebaeda281d2302a34ef7e6edd37d6f0e0a9
Becky <github@scaryclam.co.uk>
2019-02-22 12:46:31 +0000

    Updated vpc for the routing

--- aws/vpc/simple-peered/vpc.tf
+++ aws/vpc/simple-peered/vpc.tf
@@ -20,6 +20,9 @@ resource "aws_vpc" "example-vpc-2" {
 }
 
 # This is the peering stuff
+# GOTCHA ALERT! With the auto_accept value set to false, or not including it at all,
+# you will need to MANUALLY accept the connection in the AWS console!
+# If the requester and accepter are not the same owner, this will need to be done!
 resource "aws_vpc_peering_connection" "example-peer" {
     # This should be your AWS account ID. You can set this variable, either
     # by using a .tfvars file, or just by doing something like:
@@ -28,10 +31,28 @@ resource "aws_vpc_peering_connection" "example-peer" {
     peer_vpc_id = "${aws_vpc.example-vpc-1.id}"
     vpc_id = "${aws_vpc.example-vpc-2.id}"
     peer_region = "eu-west-1"
+    auto_accept = true
 }
 
 
+# If you want things in the VPCs to be able to talk to each other (and you probably do)...
+# ...and yes, you need both
+resource "aws_route" "vpc1-to-vpc2" {
+    route_table_id = "${aws_vpc.example-vpc-1.main_route_table_id}"
+    destination_cidr_block = "${aws_vpc.example-vpc-2.cidr_block}"
+    vpc_peering_connection_id = "${aws_vpc_peering_connection.example-peer.id}"
+}
+
+resource "aws_route" "vpc2-to-vpc1" {
+    route_table_id = "${aws_vpc.example-vpc-2.main_route_table_id}"
+    destination_cidr_block = "${aws_vpc.example-vpc-1.cidr_block}"
+    vpc_peering_connection_id = "${aws_vpc_peering_connection.example-peer.id}"
+}
+
+
+
 # OPTIONAL THINGS:
+# I've mainly used these to test and verify things are working as expected
 # Create an internet gateway to give our subnet access to the outside world
 resource "aws_internet_gateway" "example-1" {
     vpc_id = "${aws_vpc.example-vpc-1.id}"
